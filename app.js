4ad060af02698b2ae750824aed34f609723e49d4
Nouhayla-code <nouhayla@hotmail.dk>
2024-09-01 22:34:19 +0200

    starten

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("spillet er startet");
+}
+
+function guessTooHigh() {
+  console.log("Gættet var for højt");
+}
+
+function guessTooLow() {
+  console.log("Gættet var for lavt");
+}
+
+function correct() {
+  console.log("Gættet er korrekt ");
+}
